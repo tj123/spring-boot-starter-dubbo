2ef440982d800a016b03b8bb1ad3866dedfcaea4
dmotnxpy <dmotnxpy@gmail.com>
2016-08-24 16:58:10 +0800

    update DubboRegistrar. add DubboXmlConfiguration.

--- src/main/java/io/dubbo/springboot/DubboAutoConfiguration.java
+++ src/main/java/io/dubbo/springboot/DubboAutoConfiguration.java
@@ -1,73 +1,14 @@
 package io.dubbo.springboot;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-
-import com.alibaba.dubbo.config.ApplicationConfig;
-import com.alibaba.dubbo.config.ConsumerConfig;
-import com.alibaba.dubbo.config.ProtocolConfig;
-import com.alibaba.dubbo.config.ProviderConfig;
-import com.alibaba.dubbo.config.RegistryConfig;
-
-import io.dubbo.springboot.config.DubboProtocolConfig;
-import io.dubbo.springboot.config.DubboProviderConfig;
-import io.dubbo.springboot.config.DubboRegistryConfig;
 
 @Configuration
+@AutoConfigureAfter(DubboXmlConfiguration.class)
 @EnableConfigurationProperties(DubboProperties.class)
 @Import(DubboRegistrar.class)
 public class DubboAutoConfiguration {
-
-    @Autowired
-    private DubboProperties dubboProperties;
-
-    @Bean
-    public ApplicationConfig requestApplicationConfig() {
-        return dubboProperties.getApplication();
-    }
-
-    @Bean
-    @Primary
-    public RegistryConfig requestRegistryConfig() {
-    	RegistryConfig defaultConfig = null;
-    	List<DubboRegistryConfig> registries = dubboProperties.getRegistry();
-    	if(registries != null && !registries.isEmpty()){
-    		defaultConfig = registries.get(0);
-    	}
-        return defaultConfig;
-    }
-
-    @Bean
-    @Primary
-    public ProtocolConfig requestProtocolConfig() {
-    	ProtocolConfig defaultConfig = null;
-    	List<DubboProtocolConfig> protocols = dubboProperties.getProtocol();
-    	if(protocols != null && !protocols.isEmpty()){
-    		defaultConfig = protocols.get(0);
-    	}
-        return defaultConfig;
-    }
-
-    @Bean
-    @Primary
-    public ProviderConfig requestProviderConfig() {
-    	ProviderConfig defaultConfig = null;
-    	List<DubboProviderConfig> providers = dubboProperties.getProvider();
-    	if(providers != null && !providers.isEmpty()){
-    		defaultConfig = providers.get(0);
-    	}
-        return defaultConfig;
-    }
-    
-    @Bean
-    public ConsumerConfig requestConsumerConfig() {
-        return dubboProperties.getConsumer();
-    }
 
 }
---
+++ src/main/java/io/dubbo/springboot/DubboXmlConfiguration.java
@@ -0,0 +1,12 @@
+package io.dubbo.springboot;
+
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@AutoConfigureOrder(-1)
+@ImportResource("classpath:META-INF/spring/applicationContext.xml")
+public class DubboXmlConfiguration {
+
+}
